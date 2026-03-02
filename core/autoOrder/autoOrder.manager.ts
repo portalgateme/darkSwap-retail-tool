ba3c310b88edecc8fe25ec06bb21e0b5b91b3b7a
Peter0x2 <peter0x2@outlook.com>
2026-03-02 21:49:30 +0800

    fix recovery job from cancel

--- core/autoOrder/autoOrder.manager.ts
+++ core/autoOrder/autoOrder.manager.ts
@@ -18,6 +18,7 @@ import {
   WithdrawNoteDto
 } from '../types'
 import { SubgraphService } from '../common/subgraph.service'
+import Orders from '../../renderer/pages/history'
 
 const PRICE_DECIMALS = 18
 
@@ -774,14 +775,14 @@ export class AutoOrderManager {
     }
 
     const swapMessage = deserializeDarkSwapMessage(order.swapMessage)
-    if (
-      swapMessage &&
-      swapMessage.inNote &&
-      swapMessage.inNote.amount
-    ) {
-      const decimal = order.orderDirection === OrderDirection.SELL ? assetPair.quoteDecimal : assetPair.baseDecimal
-
-      return ethers.formatUnits(swapMessage.inNote.amount.toString(), decimal)
+    if (swapMessage) {
+      const inDecimal = order.orderDirection === OrderDirection.SELL ? assetPair.quoteDecimal : assetPair.baseDecimal
+      const outDecimal = order.orderDirection === OrderDirection.SELL ? assetPair.baseDecimal : assetPair.quoteDecimal
+      if (order.status == OrderStatus.CANCELLED) {
+        return ethers.formatUnits(swapMessage.orderNote.amount.toString(), outDecimal)
+      } else {
+        return ethers.formatUnits(swapMessage.inNote.amount.toString(), inDecimal)
+      }
     }
 
     return undefined
